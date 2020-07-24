510bc5e27b0bd23a23e2bf0616db4d9cf78df7e5
Isaac Lee <isaacrlee@gmail.com>
2020-07-23 22:07:14 -0700

    Add statcast function to TimerTrigger1

--- TimerTrigger1/__init__.py
+++ TimerTrigger1/__init__.py
@@ -1,7 +1,14 @@
 import datetime
 import logging
+import os
+import uuid
 
 import azure.functions as func
+from pybaseball import statcast
+import pymysql  # noqa: F401
+from sqlalchemy import create_engine
+
+engine = create_engine(os.environ["MYSQL_STATCAST"], pool_pre_ping=True)
 
 
 def main(mytimer: func.TimerRequest) -> None:
@@ -14,4 +21,16 @@ def main(mytimer: func.TimerRequest) -> None:
     if mytimer.past_due:
         logging.info("The timer is past due!")
 
-    logging.info("Python timer trigger function ran at %s", utc_timestamp)
+    start_dt = (datetime.date.today() - datetime.timedelta(1)).strftime(
+        "%Y-%m-%d"
+    )  # noqa: E501
+    end_dt = start_dt  # pull yesterday's statcast data
+    schema = "pitchbypitch"
+
+    df = statcast(start_dt=start_dt, end_dt=end_dt,)
+    logging.info(f"Ran statcast({start_dt}, {end_dt})")
+
+    df["id"] = df.apply(lambda row: uuid.uuid1(), axis=1).astype(str)
+
+    df.to_sql(schema, engine, if_exists="append", index=False)
+    logging.info(f"Added {len(df.index)} rows to {schema} at {utc_timestamp}")
